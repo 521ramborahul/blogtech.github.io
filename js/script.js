8f66e9d19ee4e745f3cf758284ed2f68d44d2ef6
kamlesh kumar <kamunvr522001@gmail.com>
2022-06-18 15:41:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,14 +45,10 @@ for (var i = 0; i < navLinks.length; i++) {
 }
 // counting the number of visits on website
 var n = localStorage.getItem('on_load_counter');
-
 if (n === null) {
     n = 0;
 }
-
 n++;
-
 localStorage.setItem("on_load_counter", n);
-
 document.getElementById('CounterVisitor').innerHTML = n;
 
